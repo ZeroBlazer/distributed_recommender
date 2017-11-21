f1c05c0d0db22e14f7f8e74415d4e6881a230064
Elías Josué Puma Chávez <elpumitaelias@gmail.com>
2017-11-21 12:16:31 -0500

    User based recommendation

--- libs/database/src/lib.rs
+++ libs/database/src/lib.rs
@@ -2,44 +2,48 @@ extern crate csv;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate distance;
 
 // use std::fs::File;
 use std::collections::HashMap;
+use std::collections::BTreeSet;
 
 #[derive(Debug)]
 pub struct Database {
     users: HashMap<i32, HashMap<i32, usize>>,
     movies: HashMap<i32, HashMap<i32, usize>>,
-    ratings: Vec<f32>
+    ratings: Vec<f32>,
 }
 
-// fn nearest_neighbors(db: &Database, user_id: i32, func: fn(&[f32], &[f32]) -> f32)
-//                      -> Vec<(f32, String)> {
-//     let mut dist_vec: Vec<(f32, String)> = Vec::new();
+// fn nearest_neighbors(n: usize) -> Vec<(f32, String)> {
+//         let mut dist_vec: Vec<(f32, String)> = Vec::new();
 
-//     for (rec_id, _) in db.0.iter() {
-//         if id != rec_id.as_str() {
-//             let rec_str = rec_id.clone();
-//             let (obj_vec, rec_vec) = users_rating_vectors(db, id, rec_id);
-//             dist_vec.push((func(&obj_vec, &rec_vec), rec_str));
+//         dist_vec.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+//         dist_vec
+
+//         for (rec_id, _) in db.0.iter() {
+//             if id != rec_id.as_str() {
+//                 let rec_str = rec_id.clone();
+//                 let (obj_vec, rec_vec) = users_rating_vectors(db, id, rec_id);
+//                 dist_vec.push((func(&obj_vec, &rec_vec), rec_str));
+//             }
 //         }
 //     }
-
-//     dist_vec.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-//     dist_vec
-// }
 
 impl Database {
     pub fn new() -> Database {
         Database {
             users: HashMap::new(),
             movies: HashMap::new(),
-            ratings: Vec::new()
+            ratings: Vec::new(),
         }
     }
 
     pub fn from_file(path: &str) -> Database {
-        let mut rdr = csv::ReaderBuilder::new().delimiter(b',').from_path(path).unwrap();
+        let mut rdr = csv::ReaderBuilder::new()
+            .delimiter(b',')
+            .from_path(path)
+            .unwrap();
 
         let mut users: HashMap<i32, HashMap<i32, usize>> = HashMap::new();
         let mut movies: HashMap<i32, HashMap<i32, usize>> = HashMap::new();
@@ -59,21 +63,74 @@ impl Database {
         Database {
             users: users,
             movies: movies,
-            ratings: ratings
+            ratings: ratings,
         }
     }
 
-    fn get_user_ratings(user_id: i32) -> Vec<f32> {
-        
+    fn get_ratings_from_user(&self, user_id: i32, movies: &[&i32]) -> Vec<f32> {
+        let mut ratings: Vec<f32> = Vec::new();
+
+        if let Some(user_movie_ratings) = self.users.get(&user_id) {
+            for movie in movies {
+                if let Some(rating) = user_movie_ratings.get(movie) {
+                    ratings.push(self.ratings[*rating]);
+                } else {
+                    ratings.push(0.0);
+                }
+            }
+        } else {
+            panic!("User didn't rate any movie");
+        };
+
+        ratings
     }
 
-    fn get_item_ratings(user_id: i32) -> Vec<f32> {
-
-    }
-
-    // fn user_based_recommendation(user_id: i32) -> f32 {
+    // fn get_item_ratings(user_id: i32) -> Vec<f32> {
 
     // }
+
+    fn get_users_common_ratings(&self, user_id1: i32, user_id2: i32) -> Vec<&i32> {
+        let rated_movies_us1: BTreeSet<&i32> = if let Some(ratings) = self.users.get(&user_id1) {
+            ratings.keys().collect()
+        } else {
+            BTreeSet::new()
+        };
+
+        let rated_movies_us2: BTreeSet<&i32> = if let Some(ratings) = self.users.get(&user_id2) {
+            ratings.keys().collect()
+        } else {
+            BTreeSet::new()
+        };
+
+        rated_movies_us1.union(&rated_movies_us2).cloned().collect()
+    }
+
+    pub fn user_distance_vector(&self, user_id: i32, func: fn(&[f32], &[f32]) -> f32) -> Vec<(i32, f32)> {
+        let mut dist_vec: Vec<(i32, f32)> = Vec::new();
+
+        let users: Vec<&i32> = self.users.keys().collect();
+        for user in &users {
+            if **user != user_id {
+                let common_ratings = self.get_users_common_ratings(user_id, **user);
+                let usr1_vec = self.get_ratings_from_user(user_id, &common_ratings);
+                let usr2_vec = self.get_ratings_from_user(**user, &common_ratings);
+
+                dist_vec.push((**user, func(&usr1_vec, &usr2_vec)));
+            }
+        }
+
+        dist_vec
+    }
+
+    
+
+    pub fn user_based_recommendation(&self, user_id: i32) -> Vec<(i32, f32)> {
+        let mut dist_vec = self.user_distance_vector(user_id, distance::pearson_coef);
+
+        dist_vec.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+        
+        dist_vec
+    }
 
     // fn item_based_recommendation(user_id: i32) -> f32 {
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate database;
+extern crate distance;
 
 use database::*;
+use distance::pearson_coef;
 
 fn main() {
     let db = Database::from_file("../../Downloads/ml-latest-small/ratings.csv");
-    println!("{:?}", db);
+
+    println!("{:?}", db.user_based_recommendation(1));
 }
