a1ed680c2e437e2b1c8c715d38ec1e627632fd29
Elías Puma <elpumitaelias@gmail.com>
2017-11-21 15:20:26 -0500

    Per process database

--- src/main.rs
+++ src/main.rs
@@ -38,26 +38,22 @@ fn get_params<I: Iterator<Item=String>>(args: I) -> Environment {
 }
 
 fn main() {
-    let path = "data/ml-latest-small/ratings_prueba.csv";
+    let params = get_params(std::env::args());
+    println!("{:?}", params);
+
+    let path = "data/ml-latest-small/ratings.csv";
+
     let num_records = database::count_records(path);
     println!("{}", num_records);
-    let num_workers: usize = 10;
+    
+    let i = params.process_id;
+    let num_workers = params.processes;
+
     let div_rec_work = (num_records as f32 / num_workers as f32).ceil() as usize;
 
     println!("valor {}", div_rec_work);
-    for i in (0..num_workers) {
-        let db = Database::from_file(path,(i*div_rec_work),(div_rec_work*(i+1)));
-        println!("{:#?}", db);
-    }
-/*
-    {
-        let db = Database::from_file(path,0,3);
-        println!("{:#?}", db);
-    }
-    {
-        let db = Database::from_file(path,3,6);
-        println!("{:#?}", db);
-    }
-*/
-    // println!("{:?}", db.user_based_recommendation(1));
+    // for i in (0..num_workers) {
+    let db = Database::from_file(path,(i*div_rec_work),(div_rec_work*(i+1)));
+    println!("{:#?}", db);
+    // }
 }
