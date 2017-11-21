2986c72475f7a0a8fee2fceba00eda2ee7f688e7
eduardo.macetas <eduardo.macetas@gmail.com>
2017-11-21 14:24:41 -0500

    main

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use database::*;
 use distance::pearson_coef;
 
 fn main() {
-    let db = Database::from_file("../../Downloads/ml-latest-small/ratings.csv");
+    let db = Database::from_file("data/ml-20m/ratings.csv");
 
     println!("{:?}", db.user_based_recommendation(1));
 }
