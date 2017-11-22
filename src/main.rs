d11306af60eceb2e0e86f2e846252ffb1ed1d01f
Elías Puma <elpumitaelias@gmail.com>
2017-11-22 11:38:12 -0500

    Now working with workers

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,70 @@ extern crate distance;
 extern crate timely;
 
 use database::*;
-use timely::dataflow::InputHandle;
-use timely::dataflow::operators::{Input, Exchange, Inspect, Probe};
+
+use std::collections::HashMap;
+
+use timely::dataflow::{InputHandle, ProbeHandle};
+use timely::dataflow::operators::{Input, Exchange, Map, Operator, Inspect, Probe};
+// use timely::dataflow::channels::pact::Exchange;
 
 fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
-        
-        // let params = get_params(std::env::args());
-        // println!("{:?}", params);
-
         let process_id = worker.index();
         let n_processes = worker.peers();
 
         let mut input = InputHandle::new();
 
-        println!("My process_id is: {}\npeers are: {}", process_id, n_processes);
+        println!("My process_id is: {}\nWe are {} processes", process_id, n_processes);
+
+        let path = "data/ml-latest-small/ratings.csv";
+
+        // let num_records = database::count_records(path);
+        // println!("{}", num_records);
+        
+        let i = process_id;
+        let num_workers = n_processes;
+
+        // let div_rec_work = (num_records as f32 / num_workers as f32).ceil() as usize;
+        // println!("N records by process: {}", div_rec_work);
+
+        let db = Database::from_file(path);
+        // println!("{:?}", db);
 
         // create a new input, exchange data, and inspect its output
         let probe = worker.dataflow(|scope| {
             scope.input_from(&mut input)
                  .exchange(|x| *x)
-                 .inspect(move |x| println!("worker {}:\thello {}", process_id, x))
+                 .inspect(move |x| {
+                     println!("{:?}", db.user_based_recommendation(*x as i32));
+                    //  println!("worker {}:\thello {}", process_id, x)
+                 })
                  .probe()
         });
 
         // introduce data and watch!
         if process_id == 0 {
-            for round in 0..10 {
-                input.send(round);
-                input.advance_to(round + 1);
+            // let user_id = get_input();
+
+            // Get users who have rated movies
+
+            for step in 0..n_processes {
+                // input.send((user_id, range_origin, range_end));
+                input.send(step as u64 + 1);
+                input.advance_to(step + 1);
                 while probe.less_than(input.time()) {
                     worker.step();
                 }
             }
         }
 
-        // let path = "data/ml-latest-small/ratings.csv";
-
-        // let num_records = database::count_records(path);
-        // println!("{}", num_records);
-
-        // let i = params.process_id;
-        // let num_workers = params.processes;
-
-        // let div_rec_work = (num_records as f32 / num_workers as f32).ceil() as usize;
-
-        // println!("N records by process: {}", div_rec_work);
-
-        // let db = Database::from_file(path,(i*div_rec_work),(div_rec_work*(i+1)));
-        // println!("{:?}", db);
-
     }).unwrap();
 }
+
+// let params = get_params(std::env::args());
+// println!("{:?}", params);
+
+
 
 // extern crate getopts;
 // #[derive(Debug)]
